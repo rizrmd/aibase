d551ddcd8c2a44e3f8b07d2b45196b3709be7c2d
rizky <rizky05@gmail.com>
2026-01-26 03:09:50 +0700

    chore: remove temporary extension fix scripts

--- backend/src/tools/extensions/test-backticks.js
+++
@@ -1,5 +0,0 @@
-// Test backtick concatenation
-const result = '`' + '`' + '`typescript\nconst x = 1;\n' + '`' + '`' + '`';
-console.log(result);
-console.log('---');
-console.log('Expected: ```typescript\\nconst x = 1;\\n```');
